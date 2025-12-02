3f1d827a7eeef297675e8087dc189c86b2ee4236
htl3r-2131 <2131@htl.rennweg.at>
2025-12-02 12:25:08 +0100

    file uploaded for car detection

---
+++ auto_detection.py
@@ -0,0 +1,15 @@
+from inference import InferencePipeline
+from inference.core.interfaces.stream.sinks import render_boxes
+
+ROBOFLOW_API_KEY = "c3P356etbTH7VIVpbpCk"
+MODEL_ID = "my-first-project-rj433/6"
+VIDEO_REFERENCE = 0
+
+pipeline = InferencePipeline.init(
+    model_id=MODEL_ID,
+    api_key=ROBOFLOW_API_KEY,
+    video_reference=VIDEO_REFERENCE,
+    on_prediction=render_boxes)
+
+pipeline.start()
+pipeline.join()
